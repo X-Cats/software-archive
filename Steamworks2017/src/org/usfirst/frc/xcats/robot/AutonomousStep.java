656d323da757b3c1ecf6d172a6972efddfee74ef
xcats <xcats@4ff3adab-0fa4-4da2-82b3-b6a6883fea66>
2017-01-19 00:08:16 +0000

    Added autonomous step class

---
+++ Steamworks2017/src/org/usfirst/frc/xcats/robot/AutonomousStep.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc.xcats.robot;
+
+import org.usfirst.frc.xcats.robot.AutonomousStep.stepTypes;
+
+public class AutonomousStep {
+	public enum stepTypes {DRIVE, SHOOT, GRAB, UNGRAB, LIFT, LOWER, LIFT_TO_LOW, WAIT, STOP, RELEASE, DRIVE_DISTANCE,ROTATE};
+				
+		public String name= "";
+		public double stepTime=0;
+		public double leftSpeed=0;
+		public double rightSpeed=0;
+		public stepTypes stepType;
+		public double distance=0;
+
+		public AutonomousStep(){
+			//blank empty constructor
+		}
+		
+		/* ROTATE command:
+		 * distance = degrees to rotate (negative is counterclockwise, positive is clockwise)
+		 * time, lefSpeed, rightSpeed, is ignored
+		 * 
+		 * DRIVE_DISTANCE command:
+		 * distance in ft is used
+		 * leftSpeed is used to set both drive side speed
+		 * time, rightSpeed is ignored
+		 * 
+		 * DRIVE
+		 * time = seconds to move
+		 * leftSpeed = speed of left motors
+		 * rightSpeed = speed of right motors
+		 * distance is ignored
+		 * 
+		 * LIFT, LOWER
+		 * time is the amount of time to wait for the operation to continue
+		 * leftSpeed,rightSpeed,distance is ignored		
+		*/
+		
+		
+		
+		public AutonomousStep(AutonomousStep.stepTypes stepType, String name,double time, double leftSpeed,double rightSpeed, double distance){
+			this.name = name;
+			this.stepTime = time;
+			this.leftSpeed = leftSpeed;
+			this.rightSpeed = rightSpeed;
+			this.stepType = stepType; 
+			this.distance = distance;
+		}
+}
+
