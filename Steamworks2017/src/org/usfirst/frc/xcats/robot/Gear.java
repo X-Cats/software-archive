239b5ce0f621281302edfa9c966496dbe2d785c2
xcats <xcats@4ff3adab-0fa4-4da2-82b3-b6a6883fea66>
2017-01-28 15:43:03 +0000

    Added gear and climber class

---
+++ Steamworks2017/src/org/usfirst/frc/xcats/robot/Gear.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.xcats.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class Gear {
+	//sensor = new DigitalInput(4)
+	//sensor
+	//method see if obstructed
+	//method rotate gear if blocked
+	//method place gear
+}
