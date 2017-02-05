46de6113182cf2686282f02fb8b2310f960f7f85
xcats <xcats@4ff3adab-0fa4-4da2-82b3-b6a6883fea66>
2017-02-05 21:55:57 +0000

    changes for camera and roughed in GEAR and CLIMBER classes

--- Steamworks2017/src/org/usfirst/frc/xcats/robot/AutoTarget.java
+++ Steamworks2017/src/org/usfirst/frc/xcats/robot/AutoTarget.java
@@ -4,18 +4,65 @@ import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+
+
+/*
+ * This class is designed to work with the webcam and display a targeting grid
+ * 
+ *     0,0                                                             640, 0
+ *      +----------------------------P0-----------------------------------+
+ *      |      P4_0                  .                            P5_0    |
+ *      |                            .                                    |
+ *      |                            .                                    |
+ *      |                            .                                    |
+ *      |                            .                                    |
+ *      |                            .                                    |
+ *      |                    .    "  P2  "     .                          |
+ *      |      P4 .     "                              "     .    P5      |
+ *      | .   "                                                    "    . |
+ *      |P1                                                             P3|
+ *      |                                                                 |
+ *      +-----------------------------------------------------------------+
+ *     0, 480                                                          640, 480
+ *
+ *     
+ *     We need 3 lines on the display
+ *     >  P0 is the center point of the gear target and where the rope will hang
+ *     >  P2 is the bottom of the gear target as it recedes into the distance
+ *     >  P1 is the left side bottom of the inside of the tape when the gear is on the target
+ *     >  P3 is the right side bottom of the inside of the tape when the gear is on the target
+ *     
+ *     2 more when we are in position to eject
+ *     > P4 is left inside when we are in range to eject
+ *     > P5 is right inside when we are in range to eject
+ *     
+ *     if we draw these lines on the screen, they can be used to guide the robot into position
+ *     in particular, there is no good visual reference on the field for the P0-P2 line, 
+ *     it is halfway between the tapes as represented by the verticals at P1 and P3
+ *     
+ */
 
 public class AutoTarget {
 	private UsbCamera _camera; 
 	private CvSink _cvs;
 	private CvSource _outputStream;
 	private Mat _mat;
-	
-	
+	private Point _P0 	= new Point(240,0);
+	private Point _P1 	= new Point(0,420);
+	private Point _P2 	= new Point(240,280);
+	private Point _P3 	= new Point(640,420);
+	private Point _P4 	= new Point(20,320);
+	private Point _P4_0 = new Point(20,0);
+	private Point _P5 	= new Point(620,320);
+	private Point _P5_0 = new Point(620,0);
+	private Scalar _lineColor = new Scalar(0,255,255);
+	private Scalar _ejectColor = new Scalar(0,0,255);
 	
 	public AutoTarget(){
 		
@@ -23,15 +70,21 @@ public class AutoTarget {
 			_camera = CameraServer.getInstance().startAutomaticCapture();
 			// Set the resolution
 			_camera.setResolution(640, 480);
+			//_camera.setFPS(5);
 
 			// Get a CvSink. This will capture Mats from the camera
 			_cvs = CameraServer.getInstance().getVideo();
 			
 			// Setup a CvSource. This will send images back to the Dashboard
-			_outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+			_outputStream = CameraServer.getInstance().putVideo("XCATS", 640, 480);
 
 			// Mats are very memory expensive. Lets reuse this Mat.
 			_mat = new Mat();
+
+//			SmartDashboard.putNumber("P1x", 100);
+//			SmartDashboard.putNumber("P1y", 100);
+//			SmartDashboard.putNumber("P2x", 400);
+//			SmartDashboard.putNumber("P2y", 500);
 			
 		}
 		catch (Exception e)
@@ -53,8 +106,32 @@ public class AutoTarget {
 	
 		}
 		// Put a rectangle on the image
-		Imgproc.rectangle(_mat, new Point(100, 100), new Point(400, 400),
-				new Scalar(255, 255, 255), 5);
+//		double P1x,P1y,P2x,P2y;
+//		P1x = SmartDashboard.getNumber("P1x", 50);
+//		P1y = SmartDashboard.getNumber("P1y", 50);
+//		P2x = SmartDashboard.getNumber("P2x", 450);
+//		P2y = SmartDashboard.getNumber("P2y", 550);
+		
+		//Draw the center line, this is where we want the gear and the rope
+		Imgproc.line(_mat, _P0, _P2, _lineColor, 5 , 8, 0);
+
+		//Draw the P1 to P2 segment
+		Imgproc.line(_mat, _P1, _P2, _lineColor, 3 , 8, 0);
+
+		//Draw the P2 to P3 segment
+		Imgproc.line(_mat, _P2, _P3, _lineColor, 3 , 8, 0);
+		
+		//Draw the P4 segment (to top of screen)
+		Imgproc.line(_mat, _P4, _P4_0, _ejectColor, 3 , 8, 0);
+		
+		//Draw the P5 segment (to top of screen)
+		Imgproc.line(_mat, _P5, _P5_0, _ejectColor, 3 , 8, 0);
+		
+//		Imgproc.rectangle(_mat, new Point(120, 100), new Point(450, 250),
+//				new Scalar(0, 0, 255), 5);
+//		Imgproc.rectangle(_mat, new Point(P1x, P1y), new Point(P2x, P2y),
+//				new Scalar(0, 255, 255), 5);
+		
 		// Give the output stream a new image to display
 		_outputStream.putFrame(_mat);				
 	}
--- Steamworks2017/src/org/usfirst/frc/xcats/robot/Climber.java
+++ Steamworks2017/src/org/usfirst/frc/xcats/robot/Climber.java
@@ -1,6 +1,24 @@
 package org.usfirst.frc.xcats.robot;
 
+import org.usfirst.frc.xcats.robot.XCatsSpeedController.SCType;
+
 public class Climber {
 //motor
 //method climb
+	private XCatsSpeedController _winch;
+	
+	public Climber(){
+		//constructor
+		_winch = new XCatsSpeedController("Winch",Enums.WINCH_CAN_ID,false,SCType.TALON,null,null);
+		}
+	
+	
+	public void climb(){
+		
+		_winch.set(Enums.WINCH_CLIMB_SPEED);
+		
+	}
+	public void updateStatus(){
+		
+	}
 }
