5a906e04e5b67e1a744013231aedd4ad0cf9a501
xcats <xcats@4ff3adab-0fa4-4da2-82b3-b6a6883fea66>
2017-02-08 23:11:28 +0000

    updates for double solenoid on Gear class

--- Steamworks2017/src/org/usfirst/frc/xcats/robot/Gear.java
+++ Steamworks2017/src/org/usfirst/frc/xcats/robot/Gear.java
@@ -3,25 +3,24 @@ package org.usfirst.frc.xcats.robot;
 import org.usfirst.frc.xcats.robot.XCatsSpeedController.SCType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Gear {
-	private Solenoid _sol;
+	private DoubleSolenoid _sol;
 	private DigitalInput _optoRotate;
 	private DigitalInput _optoOnBoard;
 	private XCatsSpeedController _gearRotator;
 	private boolean _acquiring = false;
-	//sensor = new DigitalInput(4)
-	//sensor
-	//method see if obstructed
-	//method rotate gear if blocked
-	//method place gear
+    private boolean _ejecting = false;
+    private Timer _ejectTimer= new Timer();
 	
 	public Gear(){
 		
 		// constructor
-		_sol = new Solenoid(Enums.PCM_CAN_ID,Enums.GEAR_PCM);
+		_sol = new DoubleSolenoid(Enums.PCM_CAN_ID,Enums.GEAR_PCM_FORWARD,Enums.GEAR_PCM_REVERSE);
 		_optoRotate = new DigitalInput(Enums.GEAR_POSITIONED_OPT);
 		_optoOnBoard = new DigitalInput(Enums.GEAR_ONBOARD_OPT);
 		_gearRotator =  new XCatsSpeedController("Gear Rotator",Enums.GEAR_ROTATOR_CAN_ID,false,SCType.TALON,null,null);
@@ -36,7 +35,12 @@ public class Gear {
 		
 	}
 	public void eject(){
-		_sol.set(true);
+		if (!_ejecting){
+			_sol.set(Value.kForward);
+			_ejecting = true;
+			_ejectTimer.reset();
+			_ejectTimer.start();
+		}
 	}
 	public boolean isOnBoard(){
 		return _optoOnBoard.get();
@@ -46,6 +50,14 @@ public class Gear {
 	public void updateStatus(){
 		SmartDashboard.putBoolean("Gear on Board",_optoOnBoard.get());
 		SmartDashboard.putBoolean("Gear Positioned", _optoRotate.get());
+		
+		if (_ejecting){
+			if (_ejectTimer.get() >= Enums.GEAR_EJECT_TIME){
+				_sol.set(Value.kReverse);
+				_ejectTimer.stop();
+				_ejecting = false;
+			}
+		}
 		
 		if (_acquiring){
 			if (_optoRotate.get())
