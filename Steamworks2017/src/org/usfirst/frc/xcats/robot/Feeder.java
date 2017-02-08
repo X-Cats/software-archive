cf30d71807c1f36d560c56753bb2f46149ae36c4
xcats <xcats@4ff3adab-0fa4-4da2-82b3-b6a6883fea66>
2017-02-08 23:52:11 +0000

    New feeder assembly

---
+++ Steamworks2017/src/org/usfirst/frc/xcats/robot/Feeder.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.xcats.robot;
+
+import org.usfirst.frc.xcats.robot.XCatsSpeedController.SCType;
+
+public class Feeder {
+	private XCatsSpeedController _topFeeder;
+	private XCatsSpeedController _bottomFeeder;
+	
+	public Feeder(){
+		
+		_topFeeder =  new XCatsSpeedController("Top Feeder",Enums.FEEDER_TOP_CAN,false,SCType.TALON,null,null);
+		_bottomFeeder =  new XCatsSpeedController("Top Feeder",Enums.FEEDER_TOP_CAN,false,SCType.TALON,null,null);
+		
+	}
+
+	public void intake(){
+		//These should be opposite directions
+		_topFeeder.set(Enums.FEEDER_INTAKE_SPEED);
+		_bottomFeeder.set( - Enums.FEEDER_INTAKE_SPEED);
+	}
+	public void lowGoal(){
+		
+		//these should be set to the SAME value
+		_topFeeder.set(Enums.FEEDER_FEED_SPEED);
+		_bottomFeeder.set(Enums.FEEDER_FEED_SPEED);
+		
+	}
+	public void feed(){
+		
+		_topFeeder.set(- Enums.FEEDER_FEED_SPEED);
+		_bottomFeeder.set(0);
+	}
+	public void stop(){
+		_topFeeder.set(0);
+		_bottomFeeder.set(0);
+	}
+	
+	public void updateStatus(){
+		
+	}
+}
