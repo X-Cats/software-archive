321219d4cb6b86e4d557926421b663bdc27fbea0
xcats <xcats@4ff3adab-0fa4-4da2-82b3-b6a6883fea66>
2017-02-11 14:18:32 +0000

    changed gear eject time to 1 sec

--- Steamworks2017/src/org/usfirst/frc/xcats/robot/Enums.java
+++ Steamworks2017/src/org/usfirst/frc/xcats/robot/Enums.java
@@ -70,7 +70,7 @@ public class Enums {
 	public static final int GEAR_ROTATOR_PWM_ID = 7;
 	public static final int GEAR_PCM_FORWARD = 7; // PCM id for the gear ejector
 	public static final int GEAR_PCM_REVERSE = 6; // PCM id for the gear ejector
-	public static final double GEAR_EJECT_TIME = 0.5; // time in seconds before retracting ejector piston
+	public static final double GEAR_EJECT_TIME = 1; // time in seconds before retracting ejector piston
 	public static final int GEAR_POSITIONED_OPT = 1; //optical sensor that detects if gear is position(rotated) correctly
 	public static final int GEAR_ONBOARD_OPT = 2;    //optical sensor that detects if the gear is positioned far enough in the robot to eject
 	public static final double GEAR_ROTATOR_SPEED = 0.25;
