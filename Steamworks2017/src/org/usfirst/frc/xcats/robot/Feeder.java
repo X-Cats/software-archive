d0bd86db784294e3043d408ee1c1fb119cf71a92
xcats <xcats@4ff3adab-0fa4-4da2-82b3-b6a6883fea66>
2017-02-12 22:35:22 +0000

    added solenoid for lifter

--- Steamworks2017/src/org/usfirst/frc/xcats/robot/Feeder.java
+++ Steamworks2017/src/org/usfirst/frc/xcats/robot/Feeder.java
@@ -2,17 +2,29 @@ package org.usfirst.frc.xcats.robot;
 
 import org.usfirst.frc.xcats.robot.XCatsSpeedController.SCType;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Feeder {
 	private XCatsSpeedController _topFeeder;
 	private XCatsSpeedController _bottomFeeder;
+	private DoubleSolenoid _lifter;
 	
 	public Feeder(){
 		
 		_topFeeder =  new XCatsSpeedController("Top Feeder",Enums.FEEDER_TOP_CAN,true,SCType.TALON,null,null);
 		_bottomFeeder =  new XCatsSpeedController("Bottom Feeder",Enums.FEEDER_BOTTOM_CAN,true,SCType.TALON,null,null);
+		//_lifter = new DoubleSolenoid(Enums.PCM_CAN_ID,Enums.FEEDER_LIFT_PCM_FORWARD,Enums.FEEDER_LIFT_PCM_BACKWARD);
 		
 	}
 
+	public void lift(){
+		_lifter.set(Value.kForward);
+	}
+	public void lower(){
+		_lifter.set(Value.kReverse);
+	}
 	public void intake(){
 		//These should be opposite directions
 		_topFeeder.set(Enums.FEEDER_INTAKE_SPEED);
@@ -36,6 +48,16 @@ public class Feeder {
 	}
 	
 	public void updateStatus(){
+		
+		if (_lifter == null)
+			return;
+		
+		boolean liftState=false;
+		
+		if (_lifter.get() == Value.kForward)
+			liftState = true;
+		
+		SmartDashboard.putBoolean("Ball Lifter is Up", liftState);
 		
 	}
 }
